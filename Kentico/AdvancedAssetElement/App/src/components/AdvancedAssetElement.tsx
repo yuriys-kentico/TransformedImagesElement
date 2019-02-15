5e9ef27771512b102f02ebd1fa0cd2f6e83ec9b4
Yuriy Sountsov <yuriys@kentico.com>
2019-02-15 00:05:21 -0500

    Try to list some assets

--- Kentico/AdvancedAssetElement/App/src/components/AdvancedAssetElement.tsx
+++ Kentico/AdvancedAssetElement/App/src/components/AdvancedAssetElement.tsx
@@ -1,6 +1,10 @@
 ﻿import * as React from "react";
+
+import { ContentManagementClient } from "kentico-cloud-content-management";
+
 import { IElement } from "./kentico/IElement";
 import { IContext } from "./kentico/IContext";
+import { string } from "prop-types";
 
 export interface IElementProps {
     element: IElement;
@@ -8,19 +12,36 @@ export interface IElementProps {
 }
 
 export interface IElementState {
-    contentManagementAPIKey: string;
+    assetURLs: string[];
 }
 
+// Expose access to Kentico custom element API
+declare const CustomElement: any;
+
 export class AdvancedAssetElement extends React.Component<IElementProps, IElementState> {
-    state = {
-        contentManagementAPIKey: this.props.element.config.contentManagementAPIKey
-    };
+    client = new ContentManagementClient({
+        projectId: this.props.context.projectId,
+        apiKey: this.props.element.config.contentManagementAPIKey
+    });
+
+    componentWillMount() {
+        this.client.listAssets()
+            .toObservable()
+            .subscribe(response => {
+                this.setState({
+                    assetURLs: response.data.items.map(i => `https://assets-us-01.kc-usercontent.com:443/${this.props.context.projectId}/${i.fileReference}/${i.fileName}"`)
+                })
+                response.data.items
+            })
+    }
 
     render() {
         return (
             <div style={{ textAlign: 'center' }}>
-                <h1>Hello Kentico, here are some assets</h1>
-                <h2>{this.state.contentManagementAPIKey}</h2>
+                <h1>Here are some assets</h1>
+                <div id="assetList">
+                    {this.state.assetURLs.map(u => <img src={u} />)}
+                </div>
             </div>);
     }
 }
--- Kentico/AdvancedAssetElement/App/src/index.tsx
+++ Kentico/AdvancedAssetElement/App/src/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+
 import { IElement } from "./components/kentico/IElement";
 import { IContext } from "./components/kentico/IContext";
 
 import { AdvancedAssetElement } from "./components/AdvancedAssetElement";
 
-declare var CustomElement: any;
+// Expose access to Kentico custom element API
+declare const CustomElement: any;
 
 CustomElement.init((element: IElement, _context: IContext) =>
     ReactDOM.render(<AdvancedAssetElement element={element} context={_context} />, document.getElementById('root')
